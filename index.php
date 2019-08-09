30f46426e87de3d0a51eb95b008cf4cce9d5e0cf
ErwinPreter1761 <erwin.technic16@gmail.com>
2019-08-09 21:02:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum eaque corrupti perspiciatis et nulla dolor facere magni! Molestias omnis dolor natus, modi, nihil, corrupti debitis voluptas velit ratione architecto voluptatem.</p>
+    <ul>
+        <li>Satu-satu aku sayang ibu</li>
+        <li>Dua-dua juga sayang ayah</li>
+        <li>Tiga-tiga sayang adik Kakak</li>
+         <li>Satu Dua Tiga Sayang semuanya</li>
+    </ul>
 </body>
 </html>
